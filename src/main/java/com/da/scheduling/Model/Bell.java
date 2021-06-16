fbf0f6b4dab07fb4828a42e588f8d8a2c83f68a9
sahar <shrsahebi@gmail.com>
2021-06-16 17:18:38 +0430

    initial commit

---
+++ src/main/java/com/da/scheduling/Model/Bell.java
@@ -0,0 +1,33 @@
+package com.da.scheduling.Model;
+
+public class Bell {
+
+
+    private int id;
+    private String label;
+    private int bellOfDay;
+
+    public int getBellOfDay() {
+        return bellOfDay;
+    }
+
+    public void setBellOfDay(int bellOfDay) {
+        this.bellOfDay = bellOfDay;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+}
